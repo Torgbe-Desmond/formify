b3224877b3dd382f9d55d5ae8be25d7b8918237e
Torgbe-Desmond <123torgbe@gmail.com>
2026-03-27 17:18:28 +0000

    adding test route for testing

---
+++ src/FastTransfers.API/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastTransfers.API.Controllers
+{
+    [Route("api/test")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        /// <summary>Testing.</summary>
+        [HttpGet("")]
+        public ActionResult<string> GetTest()
+        {   
+            var testing = "Yeah we are testing";
+            return testing;
+        }
+    }
+}
